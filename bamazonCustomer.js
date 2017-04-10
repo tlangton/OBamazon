13d76c0bca2e66235c96ac43f327359053e9d657
Tom Langton <tom@pulpinfo.com>
2017-04-10 18:35:29 -0400

    monday end of day

---
+++ bamazonCustomer.js
@@ -0,0 +1,86 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    user: "bootcamp",
+    password: "",
+    database: "bamazon"
+});
+
+//connect to the db
+connection.connect();
+
+//vars
+var inventoryQty = "";
+var requestQty = "";
+var tempId = "";
+//inital display
+var runAllProducts = function() {
+    console.log(
+        `  All IDs, Products and Prices
+		-------------------------------------------------`
+    );
+
+    connection.query("SELECT * from products", function(error, res, fields) {
+        if (error) throw error;
+        for (var i = 0; i < res.length; i++) {
+            console.log( res[i].item_id + " | " + res[i].product_name + " | " + res[i].price );
+        }
+        console.log(`-------------------------------------------------`);
+    });
+};
+
+//inquiries
+var productAddToCart = function() {
+    inquirer
+        .prompt([
+            {
+                name: "itemId",
+                type: "input",
+                message: "Enter Product ID: "
+            },
+            {
+                name: "quantity",
+                type: "input",
+                message: "Enter quantity: "
+            }
+        ])
+        .then(function(answer) {
+            // console.log(answer.itemId , answer.quantity)
+            requestQty = answer.quantity;
+            tempId = answer.itemId;
+            var query = "SELECT item_id,stock_quantity, product_name FROM products WHERE item_id = ?";
+
+            connection.query(query, [answer.itemId], function(err, res) {
+                if (err) throw err;
+                inventoryQty = res[0].stock_quantity;
+                tempProduct = res[0].product_name;
+                console.log( "Product ID: " + res[0].item_id + " || Current Inventory: " + res[0].stock_quantity);
+
+                if (requestQty > inventoryQty) {
+                    console.log("Insufficient inventory.");
+                } else {
+                    console.log("Enough in stock to fill order.");
+                    var adjustedInventory = inventoryQty - requestQty;
+
+                    //update the table
+                    var query = "UPDATE products SET stock_quantity = ? WHERE item_id = ?";
+                    connection.query(query, [adjustedInventory, tempId], function(err, res) {
+                            if (err) throw err;
+                            console.log(
+                                "Inventory has been set to " + adjustedInventory + " for Product ID " + tempId + ", " + tempProduct.trim() +"."
+                            );
+                        }
+                    );
+                }
+            });
+        });
+};
+
+
+runAllProducts();
+
+productAddToCart();
+
+       // connection.end();
